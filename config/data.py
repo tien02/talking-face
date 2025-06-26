87bd2c23d776761980db2b406dda06f3fa2cfc27
tien02 <tien.danganh02@gmail.com>
2025-06-26 23:28:25 +0700

    configs for weviate

--- config/data.py
+++ config/data.py
@@ -13,4 +13,9 @@ class DataSettings(BaseSettings):
     REDIS_PORT: int = 6379
     REDIS_DB: int = 0
 
+    VECTOR_SEARCH_HOST: str
+    VECTOR_SEARCH_PORT: int
+    VECTOR_SEARCH_ALPHA: float
+    VECTOR_SEARCH_THRESHOLD: float
+
 data_settings = DataSettings()
